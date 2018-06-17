14a1006023c827f3b700e799a2c586d28f8272cb
skyilee <1424631867@qq.com>
2018-06-17 16:07:41 +0800

    warning test

--- src/test/gitTest.java
+++ src/test/gitTest.java
@@ -3,6 +3,7 @@ package test;
 public class gitTest {
 	public static void main(String[] args) {
 		System.out.println("git test");
+		System.out.println("warning£ºLF will be replaced by CRLF in ¡Á¡Á¡Á¡Á.¡Á¡Á");
 	}
 
 }
