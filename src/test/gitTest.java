9e792a68fdf8abfdd43e47ba9aa54b7657267beb
skyilee <1424631867@qq.com>
2018-06-17 17:06:39 +0800

    understand how stage works

--- src/test/gitTest.java
+++ src/test/gitTest.java
@@ -4,6 +4,7 @@ public class gitTest {
 	public static void main(String[] args) {
 		System.out.println("git test");
 		System.out.println("warning£ºLF will be replaced by CRLF in ¡Á¡Á¡Á¡Á.¡Á¡Á");
+		System.out.println("ÔÝ´æÇøtest");
 	}
 
 }
---
+++ src/test/gitTest2.java
@@ -0,0 +1,8 @@
+package test;
+
+public class gitTest2 {
+	public static void main(String[] args){
+		System.out.println("Ôİ´æÇøtest2");
+	}
+
+}
