bface35eb3b6ed94cfbbf0e9e50ef8c7c3a6bb75
skyilee <1424631867@qq.com>
2018-06-17 19:26:39 +0800

    branch test new

--- src/test/gitTest.java
+++ src/test/gitTest.java
@@ -5,7 +5,7 @@ public class gitTest {
 		System.out.println("git test");
 		System.out.println("warning£ºLF will be replaced by CRLF in ¡Á¡Á¡Á¡Á.¡Á¡Á");
 		System.out.println("ÔÝ´æÇøtest");
-		System.out.println("change test files");
+		//System.out.println("change test files");
 		System.out.println("branch test");
 	}
 
